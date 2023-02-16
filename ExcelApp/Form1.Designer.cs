20bdf70f68b591d69c1dc862704b5dac98760167
nelogeek <lokotaevpr@gmail.com>
2023-02-16 16:30:37 +0500

    НИХУУЯ НЕ ПОМЕНЯЛОССЬ

--- ExcelApp/Form1.Designer.cs
+++ ExcelApp/Form1.Designer.cs
@@ -47,16 +47,18 @@
             // labelNameFolder
             // 
             this.labelNameFolder.AutoSize = true;
-            this.labelNameFolder.Location = new System.Drawing.Point(13, 19);
+            this.labelNameFolder.Location = new System.Drawing.Point(17, 23);
+            this.labelNameFolder.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelNameFolder.Name = "labelNameFolder";
-            this.labelNameFolder.Size = new System.Drawing.Size(0, 15);
+            this.labelNameFolder.Size = new System.Drawing.Size(0, 16);
             this.labelNameFolder.TabIndex = 1;
             // 
             // btnSelectFolder
             // 
-            this.btnSelectFolder.Location = new System.Drawing.Point(370, 285);
+            this.btnSelectFolder.Location = new System.Drawing.Point(493, 351);
+            this.btnSelectFolder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSelectFolder.Name = "btnSelectFolder";
-            this.btnSelectFolder.Size = new System.Drawing.Size(146, 23);
+            this.btnSelectFolder.Size = new System.Drawing.Size(195, 28);
             this.btnSelectFolder.TabIndex = 3;
             this.btnSelectFolder.Text = "Выбрать папку";
             this.btnSelectFolder.UseVisualStyleBackColor = true;
@@ -65,16 +67,18 @@
             // labelCompleted
             // 
             this.labelCompleted.AutoSize = true;
-            this.labelCompleted.Location = new System.Drawing.Point(13, 290);
+            this.labelCompleted.Location = new System.Drawing.Point(17, 357);
+            this.labelCompleted.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelCompleted.Name = "labelCompleted";
-            this.labelCompleted.Size = new System.Drawing.Size(0, 15);
+            this.labelCompleted.Size = new System.Drawing.Size(0, 16);
             this.labelCompleted.TabIndex = 7;
             // 
             // btnBuild
             // 
-            this.btnBuild.Location = new System.Drawing.Point(555, 285);
+            this.btnBuild.Location = new System.Drawing.Point(740, 351);
+            this.btnBuild.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBuild.Name = "btnBuild";
-            this.btnBuild.Size = new System.Drawing.Size(75, 23);
+            this.btnBuild.Size = new System.Drawing.Size(100, 28);
             this.btnBuild.TabIndex = 5;
             this.btnBuild.Text = "Собрать";
             this.btnBuild.UseVisualStyleBackColor = true;
@@ -82,8 +86,8 @@
             // 
             // StartNumberNumeric
             // 
-            this.StartNumberNumeric.Location = new System.Drawing.Point(151, 57);
-            this.StartNumberNumeric.Margin = new System.Windows.Forms.Padding(3, 3, 10, 10);
+            this.StartNumberNumeric.Location = new System.Drawing.Point(201, 70);
+            this.StartNumberNumeric.Margin = new System.Windows.Forms.Padding(4, 4, 13, 12);
             this.StartNumberNumeric.Maximum = new decimal(new int[] {
             999999999,
             0,
@@ -95,7 +99,7 @@
             0,
             0});
             this.StartNumberNumeric.Name = "StartNumberNumeric";
-            this.StartNumberNumeric.Size = new System.Drawing.Size(57, 20);
+            this.StartNumberNumeric.Size = new System.Drawing.Size(76, 22);
             this.StartNumberNumeric.TabIndex = 10;
             this.StartNumberNumeric.Value = new decimal(new int[] {
             1,
@@ -106,48 +110,51 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 59);
+            this.label1.Location = new System.Drawing.Point(17, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 15);
+            this.label1.Size = new System.Drawing.Size(167, 16);
             this.label1.TabIndex = 11;
             this.label1.Text = "Номер первой страницы";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 89);
+            this.label3.Location = new System.Drawing.Point(17, 110);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(143, 15);
+            this.label3.Size = new System.Drawing.Size(158, 16);
             this.label3.TabIndex = 15;
             this.label3.Text = "Стр. после содержания";
             // 
             // afterTitleNumeric
             // 
             this.afterTitleNumeric.Enabled = false;
-            this.afterTitleNumeric.Location = new System.Drawing.Point(151, 88);
-            this.afterTitleNumeric.Margin = new System.Windows.Forms.Padding(3, 3, 10, 10);
+            this.afterTitleNumeric.Location = new System.Drawing.Point(201, 108);
+            this.afterTitleNumeric.Margin = new System.Windows.Forms.Padding(4, 4, 13, 12);
             this.afterTitleNumeric.Maximum = new decimal(new int[] {
             999999999,
             0,
             0,
             0});
             this.afterTitleNumeric.Name = "afterTitleNumeric";
-            this.afterTitleNumeric.Size = new System.Drawing.Size(57, 20);
+            this.afterTitleNumeric.Size = new System.Drawing.Size(76, 22);
             this.afterTitleNumeric.TabIndex = 14;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(14, 120);
+            this.label4.Location = new System.Drawing.Point(19, 148);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(87, 15);
+            this.label4.Size = new System.Drawing.Size(96, 16);
             this.label4.TabIndex = 17;
             this.label4.Text = "Страниц в ПЗ";
             // 
             // CountPagePZNumeric
             // 
-            this.CountPagePZNumeric.Location = new System.Drawing.Point(151, 119);
-            this.CountPagePZNumeric.Margin = new System.Windows.Forms.Padding(3, 3, 10, 10);
+            this.CountPagePZNumeric.Location = new System.Drawing.Point(201, 146);
+            this.CountPagePZNumeric.Margin = new System.Windows.Forms.Padding(4, 4, 13, 12);
             this.CountPagePZNumeric.Maximum = new decimal(new int[] {
             999999999,
             0,
@@ -159,7 +166,7 @@
             0,
             0});
             this.CountPagePZNumeric.Name = "CountPagePZNumeric";
-            this.CountPagePZNumeric.Size = new System.Drawing.Size(57, 20);
+            this.CountPagePZNumeric.Size = new System.Drawing.Size(76, 22);
             this.CountPagePZNumeric.TabIndex = 16;
             this.CountPagePZNumeric.Value = new decimal(new int[] {
             1,
@@ -170,9 +177,10 @@
             // TwoSidedPrintCheckBox
             // 
             this.TwoSidedPrintCheckBox.AutoSize = true;
-            this.TwoSidedPrintCheckBox.Location = new System.Drawing.Point(16, 211);
+            this.TwoSidedPrintCheckBox.Location = new System.Drawing.Point(21, 260);
+            this.TwoSidedPrintCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TwoSidedPrintCheckBox.Name = "TwoSidedPrintCheckBox";
-            this.TwoSidedPrintCheckBox.Size = new System.Drawing.Size(149, 19);
+            this.TwoSidedPrintCheckBox.Size = new System.Drawing.Size(163, 20);
             this.TwoSidedPrintCheckBox.TabIndex = 19;
             this.TwoSidedPrintCheckBox.Text = "Двустронняя печать";
             this.TwoSidedPrintCheckBox.UseMnemonic = false;
@@ -189,10 +197,10 @@
             this.SplitBookContentCheckBox.AutoSize = true;
             this.SplitBookContentCheckBox.Checked = true;
             this.SplitBookContentCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.SplitBookContentCheckBox.Location = new System.Drawing.Point(16, 234);
-            this.SplitBookContentCheckBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.SplitBookContentCheckBox.Location = new System.Drawing.Point(21, 288);
+            this.SplitBookContentCheckBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.SplitBookContentCheckBox.Name = "SplitBookContentCheckBox";
-            this.SplitBookContentCheckBox.Size = new System.Drawing.Size(161, 19);
+            this.SplitBookContentCheckBox.Size = new System.Drawing.Size(176, 20);
             this.SplitBookContentCheckBox.TabIndex = 20;
             this.SplitBookContentCheckBox.Text = "Содержание отдельно";
             this.SplitBookContentCheckBox.UseVisualStyleBackColor = true;
@@ -200,20 +208,21 @@
             // infoTextBox
             // 
             this.infoTextBox.BackColor = System.Drawing.SystemColors.Menu;
-            this.infoTextBox.Location = new System.Drawing.Point(220, 57);
-            this.infoTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.infoTextBox.Location = new System.Drawing.Point(293, 70);
+            this.infoTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.infoTextBox.Multiline = true;
             this.infoTextBox.Name = "infoTextBox";
             this.infoTextBox.ReadOnly = true;
             this.infoTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.infoTextBox.Size = new System.Drawing.Size(411, 212);
+            this.infoTextBox.Size = new System.Drawing.Size(547, 260);
             this.infoTextBox.TabIndex = 21;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(346, 47);
+            this.button1.Location = new System.Drawing.Point(461, 58);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.Size = new System.Drawing.Size(11, 10);
             this.button1.TabIndex = 22;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -221,22 +230,24 @@
             // toggleButton1
             // 
             this.toggleButton1.AutoSize = true;
-            this.toggleButton1.Location = new System.Drawing.Point(31, 330);
-            this.toggleButton1.MinimumSize = new System.Drawing.Size(45, 22);
+            this.toggleButton1.Location = new System.Drawing.Point(22, 323);
+            this.toggleButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.toggleButton1.MinimumSize = new System.Drawing.Size(60, 27);
             this.toggleButton1.Name = "toggleButton1";
             this.toggleButton1.OffBackColor = System.Drawing.Color.Gray;
             this.toggleButton1.OffToggleColor = System.Drawing.Color.Gainsboro;
             this.toggleButton1.OnBackColor = System.Drawing.Color.MediumSlateBlue;
             this.toggleButton1.OnToggleColor = System.Drawing.Color.WhiteSmoke;
-            this.toggleButton1.Size = new System.Drawing.Size(45, 22);
+            this.toggleButton1.Size = new System.Drawing.Size(60, 27);
             this.toggleButton1.TabIndex = 23;
             this.toggleButton1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(136, 351);
+            this.button2.Location = new System.Drawing.Point(131, 196);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 24;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
@@ -244,10 +255,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(856, 398);
+            this.ClientSize = new System.Drawing.Size(862, 398);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.toggleButton1);
             this.Controls.Add(this.button1);
@@ -265,6 +276,7 @@
             this.Controls.Add(this.btnSelectFolder);
             this.Controls.Add(this.labelNameFolder);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Smeta++";
